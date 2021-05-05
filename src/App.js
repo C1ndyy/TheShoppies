b15d5553c07f8e6f7cbdb43c61e8338114cc2569
C1ndyy <cindx.xu@gmail.com>
2021-05-05 17:34:22 -0600

    search function

--- src/App.js
+++ src/App.js
@@ -6,10 +6,26 @@ import Nominations from "./components/Nominations/Nominations";
 import SearchBar from "./components/SearchBar/SearchBar";
 import SearchResults from "./components/SearchResults/SearchResults";
 import MobileFooter from "./components/MobileFooter/MobileFooter";
+let movies = [
+  { name: "Movie1", year: 1998, id: 1 },
+  { name: "Movie2", year: 1928, id: 2 },
+  { name: "Movie3", year: 1988, id: 3 },
+  { name: "Movie4", year: 2001, id: 4 },
+];
 
 function App() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
   const [showNominations, setShowNominations] = useState(false);
+  const [nominations, setNominations] = useState([]);
+
+  function removeNomination(id) {
+    setNominations(nominations.filter((movie) => movie.id !== id));
+  }
+
+  function addNomination(movie) {
+    setNominations([...nominations, movie]);
+  }
+
   return (
     <div className="App">
       {isMobile ? (
@@ -18,9 +34,14 @@ function App() {
           <div className="content">
             <div className="search">
               <SearchBar />
-              <SearchResults />
+              <SearchResults setShowNominations={setShowNominations} />
             </div>
-            {showNominations && <Nominations />}
+            {showNominations && (
+              <Nominations
+                nominations={nominations}
+                setNominations={setNominations}
+              />
+            )}
             <MobileFooter setShowNominations={setShowNominations} />
           </div>
         </>
@@ -30,9 +51,12 @@ function App() {
           <div className="content">
             <div className="search">
               <SearchBar />
-              <SearchResults />
+              <SearchResults setShowNominations={setShowNominations} />
             </div>
-            <Nominations />
+            <Nominations
+              nominations={nominations}
+              setNominations={setNominations}
+            />
           </div>
         </>
       )}
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./SearchBar.css";
 
 function SearchBar() {
+  const [input, setInput] = useState("");
+  const BASE_URL = "http://www.omdbapi.com/?apikey=a6e00c90";
+
+  async function findMovies(e) {
+    e.preventDefault();
+
+    try {
+      let response = await fetch(`${BASE_URL}&s=${input}&type=movie&page=1`);
+      let movies = await response.json();
+      console.log("movies:", movies);
+    } catch (error) {}
+  }
   return (
-    <div className="SearchBar">
-      <input type="text" placeholder="search movies.." />
+    <form className="SearchBar" onSubmit={findMovies}>
+      <input
+        type="text"
+        placeholder="search movies.."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
       <button>
         <span className="material-icons">search</span>
       </button>
-    </div>
+    </form>
   );
 }
 
