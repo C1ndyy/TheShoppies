fcb130b50733570c8e60ea9405f8d4a89d074310
C1ndyy <cindx.xu@gmail.com>
2021-05-05 14:25:53 -0600

    mobile components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 import Header from "./components/Header/Header";
 import Nominations from "./components/Nominations/Nominations";
 import SearchBar from "./components/SearchBar/SearchBar";
 import SearchResults from "./components/SearchResults/SearchResults";
+import MobileFooter from "./components/MobileFooter/MobileFooter";
 
 function App() {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const [showNominations, setShowNominations] = useState(false);
   return (
     <div className="App">
-      <Header />
-      <div className="content">
-        <div className="search">
-          <SearchBar />
-          <SearchResults />
-        </div>
-        <Nominations />
-      </div>
+      {isMobile ? (
+        <>
+          <Header />
+          <div className="content">
+            <div className="search">
+              <SearchBar />
+              <SearchResults />
+            </div>
+            {showNominations && <Nominations />}
+            <MobileFooter setShowNominations={setShowNominations} />
+          </div>
+        </>
+      ) : (
+        <>
+          <Header />
+          <div className="content">
+            <div className="search">
+              <SearchBar />
+              <SearchResults />
+            </div>
+            <Nominations />
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/MobileFooter/MobileFooter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./MobileFooter.css";
+
+function MobileFooter() {
+  return (
+    <div className="MobileFooter">
+      <button>View My Nominations</button>
+    </div>
+  );
+}
+
+export default MobileFooter;
